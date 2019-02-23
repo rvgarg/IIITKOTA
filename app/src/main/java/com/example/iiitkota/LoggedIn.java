3c962987df29fed0dac080fea94ad2ad20a92f60
rvgarg <raghav.garg175@gmail.com>
2019-02-23 17:48:46 +0530

    Errors resolved till now

--- app/src/main/java/com/example/iiitkota/LoggedIn.java
+++ app/src/main/java/com/example/iiitkota/LoggedIn.java
@@ -17,15 +17,14 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class LoggedIn extends AppCompatActivity {
-    FirebaseDatabase database = FirebaseDatabase.getInstance();
-    //    DatabaseReference myRef = database.getReference();
+
     private FirebaseAuth mAuth;
     private DrawerLayout drawerLayout;
     private Spinner year;
     private Spinner subject;
+    private Spinner section;
     private Button submit;
     private String access;
     private NavigationView nav_View;
@@ -34,19 +33,28 @@ public class LoggedIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
+        //Initializing the drawer layout
         drawerLayout = findViewById(R.id.drawer);
+
+        //Getting firebase authentication instance
         mAuth = FirebaseAuth.getInstance();
+
+        //Initializing the navigation view
         nav_View = findViewById(R.id.nav_view);
+
+        //Setting up the listener on navigation view
         nav_View.setNavigationItemSelectedListener(menuItem -> {
             menuItem.setChecked(true);
             int id = menuItem.getItemId();
             switch (id) {
                 case R.id.logout:
+
+                    //Signing out the user
                     mAuth.signOut();
                     startActivity(new Intent(LoggedIn.this, MainActivity.class));
                     break;
                 case R.id.exit:
-//                    android.os.Process.killProcess(android.os.Process.myPid());
+//                   Exiting the application
                     Intent homeIntent = new Intent(Intent.ACTION_MAIN);
                     homeIntent.addCategory(Intent.CATEGORY_HOME);
                     homeIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
@@ -59,7 +67,7 @@ public class LoggedIn extends AppCompatActivity {
 
         //Initialization of spinners
         year = findViewById(R.id.year);
-        Spinner section = findViewById(R.id.section);
+        section = findViewById(R.id.section);
         subject = findViewById(R.id.subject);
 
         //Setting up toolbar
@@ -85,15 +93,16 @@ public class LoggedIn extends AppCompatActivity {
         year.setAdapter(Years);
         //Setting on click listener on years spinner
         year.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-
                                            @Override
                                            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                                                section.setAdapter(Sections);
+                                               subject.setAdapter(null);
+                                               subChoser();
                                            }
 
                                            @Override
                                            public void onNothingSelected(AdapterView<?> parent) {
-                                               Toast.makeText(LoggedIn.this, "Select a year", Toast.LENGTH_LONG).show();
+//                                             Toast.makeText(LoggedIn.this, "Select a year", Toast.LENGTH_LONG).show();
                                            }
                                        }
         );
@@ -101,89 +110,26 @@ public class LoggedIn extends AppCompatActivity {
 
         //Applying item selection listeners
         section.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            ArrayAdapter<CharSequence> Subjects;
-
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                switch (position) {
-                    case 0:
-                        Toast.makeText(LoggedIn.this, "Please select a Section", Toast.LENGTH_LONG).show();
-                        break;
-                    case 1:
-                        switch (year.getSelectedItemPosition()) {
-                            case 0:
-                                Toast.makeText(LoggedIn.this, "Select a year", Toast.LENGTH_LONG).show();
-                                break;
-                            case 1:
-                                Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.csforth, android.R.layout.simple_spinner_item);
-                                Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                                subject.setAdapter(Subjects);
-                                access = "2015cp";
-                                break;
-                            case 2:
-                                Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.csthird, android.R.layout.simple_spinner_item);
-                                Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                                subject.setAdapter(Subjects);
-                                access = "2016cp";
-                                break;
-                            case 3:
-                                Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.cssecond, android.R.layout.simple_spinner_item);
-                                Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                                subject.setAdapter(Subjects);
-                                access = "2017cp";
-                                break;
-                            case 4:
-                                Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.first, android.R.layout.simple_spinner_item);
-                                Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                                subject.setAdapter(Subjects);
-                                access = "2018cp";
-                                break;
-                        }
-                        break;
-                    case 2:
-                        switch (year.getSelectedItemPosition()) {
-                            case 0:
-                                Toast.makeText(LoggedIn.this, "Select a year", Toast.LENGTH_LONG).show();
-                                break;
-                            case 1:
-                                Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.ecforth, android.R.layout.simple_spinner_item);
-                                Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                                subject.setAdapter(Subjects);
-                                access = "2015ec";
-                                break;
-                            case 2:
-                                Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.ecthird, android.R.layout.simple_spinner_item);
-                                Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                                subject.setAdapter(Subjects);
-                                access = "2016ec";
-                                break;
-                            case 3:
-                                Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.ecsecond, android.R.layout.simple_spinner_item);
-                                Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                                subject.setAdapter(Subjects);
-                                access = "2017ec";
-                                break;
-                            case 4:
-                                Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.first, android.R.layout.simple_spinner_item);
-                                Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                                subject.setAdapter(Subjects);
-                                access = "2018ec";
-                                break;
-                        }
-                        break;
-                }
+                subChoser();
             }
-
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                Toast.makeText(LoggedIn.this, "Select a section", Toast.LENGTH_LONG).show();
+//                Toast.makeText(LoggedIn.this, "Select a section", Toast.LENGTH_LONG).show();
             }
         });
 
         //Initializing and setting on click listener on the submit button
         submit = findViewById(R.id.submit);
         submit.setOnClickListener(v -> {
-
+            if (subject.getSelectedItemPosition() != 0) {
+                Intent intent = new Intent(LoggedIn.this, Marks.class);
+                intent.putExtra("Database Referance key", access);
+                startActivity(intent);
+            } else {
+                Toast.makeText(this, "Please select a subject !!!", Toast.LENGTH_LONG).show();
+            }
         });
     }
 
@@ -199,6 +145,73 @@ public class LoggedIn extends AppCompatActivity {
     }
 
     public void subChoser() {
-
+        ArrayAdapter<CharSequence> Subjects;
+        switch (section.getSelectedItemPosition()) {
+            case 0:
+                Toast.makeText(LoggedIn.this, "Please select a Section", Toast.LENGTH_LONG).show();
+                break;
+            case 1:
+                switch (year.getSelectedItemPosition()) {
+                    case 0:
+                        Toast.makeText(LoggedIn.this, "Select a year", Toast.LENGTH_LONG).show();
+                        break;
+                    case 1:
+                        Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.csforth, android.R.layout.simple_spinner_item);
+                        Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        subject.setAdapter(Subjects);
+                        access = "2015cp";
+                        break;
+                    case 2:
+                        Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.csthird, android.R.layout.simple_spinner_item);
+                        Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        subject.setAdapter(Subjects);
+                        access = "2016cp";
+                        break;
+                    case 3:
+                        Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.cssecond, android.R.layout.simple_spinner_item);
+                        Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        subject.setAdapter(Subjects);
+                        access = "2017cp";
+                        break;
+                    case 4:
+                        Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.first, android.R.layout.simple_spinner_item);
+                        Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        subject.setAdapter(Subjects);
+                        access = "2018cp";
+                        break;
+                }
+                break;
+            case 2:
+                switch (year.getSelectedItemPosition()) {
+                    case 0:
+                        Toast.makeText(LoggedIn.this, "Select a year", Toast.LENGTH_LONG).show();
+                        break;
+                    case 1:
+                        Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.ecforth, android.R.layout.simple_spinner_item);
+                        Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        subject.setAdapter(Subjects);
+                        access = "2015ec";
+                        break;
+                    case 2:
+                        Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.ecthird, android.R.layout.simple_spinner_item);
+                        Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        subject.setAdapter(Subjects);
+                        access = "2016ec";
+                        break;
+                    case 3:
+                        Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.ecsecond, android.R.layout.simple_spinner_item);
+                        Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        subject.setAdapter(Subjects);
+                        access = "2017ec";
+                        break;
+                    case 4:
+                        Subjects = ArrayAdapter.createFromResource(LoggedIn.this, R.array.first, android.R.layout.simple_spinner_item);
+                        Subjects.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                        subject.setAdapter(Subjects);
+                        access = "2018ec";
+                        break;
+                }
+                break;
+        }
     }
 }
--- app/src/main/java/com/example/iiitkota/MainActivity.java
+++ app/src/main/java/com/example/iiitkota/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.iiitkota;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.InputType;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -23,33 +26,54 @@ public class MainActivity extends AppCompatActivity {
         //Initializing Edittexts
         user = findViewById(R.id.userid);
         pwd = findViewById(R.id.Password);
+
         //Initializing forgot password Textview
         TextView frgt = findViewById(R.id.btnforgotpassword);
+
+//        Initializing login Button
         Button lgin = findViewById(R.id.btnLogin);
+
+        //Getting authentication instance
         mAuth = FirebaseAuth.getInstance();
+
+        //Setting up on click listener on the login button
         lgin.setOnClickListener(v -> {
+
+            //Declaring the variables for email address and password
             String email, password;
-            email = user.getText().toString();
-            password = pwd.getText().toString();
+
+            //Fetching the user inputted email address and password
+            email = user.getText().toString().trim();
+            password = pwd.getText().toString().trim();
+
+            //Checking if the email address or password is empty
             if (email.isEmpty() || password.isEmpty()) {
+
+                //Showing message to hte user
                 Toast.makeText(MainActivity.this, "Enter email id and password", Toast.LENGTH_LONG).show();
             } else {
+
+                //Sending request for the verification email address and password
                 mAuth.signInWithEmailAndPassword(email, password)
                         .addOnCompleteListener(this, task -> {
                             if (task.isSuccessful()) {
+                                //Getting the current user information
                                 FirebaseUser user = mAuth.getCurrentUser();
-                                updateUI(user);
+                                //Updating the activity according to the user
+                                updateUI(user, task.getResult().getAdditionalUserInfo().isNewUser());
                             } else {
                                 // If sign in fails, display a message to the user.
                                 Toast.makeText(MainActivity.this, "Authentication failed.",
                                         Toast.LENGTH_SHORT).show();
                             }
-
-                            // ...
                         });
             }
         });
+
+        //Applying on click listener to the forgot password textview
         frgt.setOnClickListener(v -> {
+
+            //Changing activity to the forgot password activity
             Intent intent = new Intent(MainActivity.this, forgotPassword.class);
             startActivity(intent);
         });
@@ -62,11 +86,80 @@ public class MainActivity extends AppCompatActivity {
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
         if (currentUser != null)
-            updateUI(currentUser);
+            updateUI(currentUser, false);
     }
 
-    private void updateUI(FirebaseUser currentUser) {
-        Intent intent = new Intent(MainActivity.this, LoggedIn.class);
-        startActivity(intent);
+    private void updateUI(FirebaseUser currentUser, boolean bool) {
+//    if(Long.parseLong(String.valueOf(currentUser.getMetadata().getLastSignInTimestamp())) == NULL)
+        //Checking if the user is new or not
+        if (bool) {
+
+            //Declaring 3 linear layout for building alert box to change password for time users
+            LinearLayout layout = new LinearLayout(this);
+            LinearLayout lpass = new LinearLayout(this);
+            LinearLayout lcnf = new LinearLayout(this);
+
+            //Declaring Alert dialog box
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            //Declaring EditTexts and TextViews for the alert dialog box
+            final EditText pass = new EditText(this);
+            final EditText cnf = new EditText(this);
+            final TextView tpass = new TextView(this);
+            final TextView tcnf = new TextView(this);
+
+            //setting linear layouts orientations
+            lpass.setOrientation(LinearLayout.HORIZONTAL);
+            lcnf.setOrientation(LinearLayout.HORIZONTAL);
+            layout.setOrientation(LinearLayout.VERTICAL);
+
+            //Setting input types on EditTexts
+            pass.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+            cnf.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+            //Setting text to the TextViews
+            tpass.setText("Enter Password");
+            tcnf.setText("Confirm Password");
+
+            //Adding TextViews and EditTexts to the following LinearLayouts
+            lpass.addView(tpass);
+            lpass.addView(pass);
+            lcnf.addView(tcnf);
+            lcnf.addView(cnf);
+
+            //Adding LinearLayouts to the root layout
+            layout.addView(lpass);
+            layout.addView(lcnf);
+
+            //Setting root layout to AlertDialog
+            builder.setView(layout);
+
+            //Adding the ok or positive button to the AlertDialog
+            builder.setPositiveButton("Ok", (dialog, which) -> {
+                String Pass = pass.getText().toString().trim(), Cnf = cnf.getText().toString().trim();
+
+                //Checking if both the passwords entered matches or not
+                if (Pass.equals(Cnf)) {
+                    currentUser.updatePassword(Pass)
+                            .addOnCompleteListener(task -> {
+                                if (task.isSuccessful()) {
+
+                                    //Changing Activity after password is changed
+                                    Intent intent = new Intent(MainActivity.this, LoggedIn.class);
+                                    startActivity(intent);
+                                }
+                            });
+                } else {
+
+                    //Message to the user when passwords entered doesn't match
+                    Toast.makeText(this, "Password didnt match", Toast.LENGTH_LONG).show();
+                }
+            });
+        } else {
+
+            //Changing activity when user is old
+            Intent intent = new Intent(MainActivity.this, LoggedIn.class);
+            startActivity(intent);
+        }
     }
 }
